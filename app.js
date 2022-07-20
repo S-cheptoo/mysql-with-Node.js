94e9e4949af5d9c59016602bb001feca024a8228
Sandra <cheptoosandra.t@gmail.com>
2022-07-20 21:38:28 +0300

    Created Posts Table

--- app.js
+++ app.js
@@ -19,6 +19,26 @@ db.connect((err) => {
 
 const app = express();
 
+//create DB
+app.get('/createdb', (req, res) => {
+    let sql = 'CREATE DATABASE nodemysql';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Database created...');
+    });
+});
+
+//Create table
+app.get('/createpoststable', (req, res) => {
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Posts table created...');
+    });
+});
+
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
